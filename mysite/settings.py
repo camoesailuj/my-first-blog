5f14a87b1e7034b3a3c9d94e225d0e11700d9ca8
camoesailuj <camoesalves.julia@gmail.com>
2017-08-26 14:27:03 -0300

    Alterando settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o%g5#rzpk^=)9f*52k6+p7=uw01cv%f9=4#p3!g(2@i52dy)k('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['camoesailuj.pythonanywhere.com']
 
 
 # Application definition
